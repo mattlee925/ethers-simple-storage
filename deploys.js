bc7f2e8938e1ba140bbb034031f5c61df0173613
mattlee925 <mattlee925@proton.me>
2022-06-29 18:45:42 -0400

    sleepy

--- deploys.js
+++ deploys.js
@@ -1,30 +1,30 @@
-const ethers = require("ethers");
-const fs = require("fs-extra");
+const ethers = require("ethers")
+const fs = require("fs-extra")
+require("dotenv").config()
 
 async function main() {
   const provider = new ethers.providers.JsonRpcBatchProvider(
-    "http://127.0.0.1:7545"
-  );
-  // next time env file
-  const wallet = new ethers.Wallet(
-    "b9a76ef180101afd9d3d3105a095ebb01e892a7d9d3bed657734a8648d637626",
-    provider
-  );
-  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+    process.env.PRC_URL
+  )
+
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8")
   const binary = fs.readFileSync(
     "./SimpleStorage_sol_SimpleStorage.bin",
     "utf8"
-  );
+  )
 
-  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
-  console.log("Deploying, please wait...");
-  const contract = await contractFactory.deploy();
-  console.log(contract);
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
+  console.log("Deploying, please wait...")
+  const contract = await contractFactory.deploy()
+  await contract.deployTransaction.wait(1)
+
+  const currentFavoriteNumber = await contract.retrieve()
 }
 
 main()
   .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
